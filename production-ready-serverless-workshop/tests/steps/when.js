78f9721a52297510654b7918f4de272dda662591
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-02-02 17:11:07 +0100

    Prepared for acceptance tests

--- production-ready-serverless-workshop/tests/steps/when.js
+++ production-ready-serverless-workshop/tests/steps/when.js
@@ -1,5 +1,9 @@
 const APP_ROOT = '../../'
 const _ = require('lodash')
+const aws4 = require('aws4')
+const URL = require('url')
+const http = require('axios')
+const mode = process.env.TEST_MODE
 
 const viaHandler = async (event, functionName) => {
     const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
@@ -13,8 +17,28 @@ const viaHandler = async (event, functionName) => {
     return response
 }
 
-const we_invoke_get_index = () => viaHandler({}, 'get-index')
+const we_invoke_get_index = async () => {
+    switch (mode) {
+        case 'handler':
+            return await viaHandler({}, 'get-index')
+        case 'http':
+            return await viaHttp('', 'GET')
+        default:
+            throw new Error(`unsupported mode: ${mode}`)
+    }
+}
+
+const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants')
+
+const we_invoke_search_restaurants = theme => {
+    let event = {
+        body: JSON.stringify({ theme })
+    }
+    return viaHandler(event, 'search-restaurants')
+}
 
 module.exports = {
-    we_invoke_get_index
+    we_invoke_get_index,
+    we_invoke_get_restaurants,
+    we_invoke_search_restaurants
 }
