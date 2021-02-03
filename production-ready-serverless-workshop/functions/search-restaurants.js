7af0eafebe90168e29b45d4968997c9894786d63
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-02-03 10:59:34 +0100

    Loading SSM parameters at runtime

--- production-ready-serverless-workshop/functions/search-restaurants.js
+++ production-ready-serverless-workshop/functions/search-restaurants.js
@@ -1,7 +1,11 @@
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 const dynamodb = new DocumentClient()
 
-const defaultResults = process.env.defaultResults || 8
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
+
+const { serviceName, stage } = process.env
+
 const tableName = process.env.restaurants_table
 
 const findRestaurantsByTheme = async (theme, count) => {
@@ -18,14 +22,24 @@ const findRestaurantsByTheme = async (theme, count) => {
     return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(async (event, context) => {
     const req = JSON.parse(event.body)
     const theme = req.theme
-    const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+    const restaurants = await findRestaurantsByTheme(theme, process.env.defaultResults)
     const response = {
         statusCode: 200,
         body: JSON.stringify(restaurants)
     }
 
     return response
-}
+}).use(ssm({
+    cache: true,
+    cacheExpiryInMillis: 1 * 60 * 1000, // 1 mins
+    names: {
+        config: `/${serviceName}/${stage}/search-restaurants/config`
+    },
+    onChange: () => {
+        const config = JSON.parse(process.env.config)
+        process.env.defaultResults = config.defaultResults
+    }
+}))
