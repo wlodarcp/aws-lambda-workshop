fa8cbda952f532155c34af491dea7e19c06f850c
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-02-03 14:48:33 +0100

    Added check for EventBridge messages in acceptance test

--- production-ready-serverless-workshop/tests/messages.js
+++ production-ready-serverless-workshop/tests/messages.js
@@ -39,6 +39,12 @@ const startListening = () => {
                     source: body.TopicArn,
                     message: body.Message
                 })
+            } else if (body.eventBusName) {
+                messages.next({
+                    sourceType: 'eventbridge',
+                    source: body.eventBusName,
+                    message: JSON.stringify(body.event)
+                })
             }
         })
 
--- production-ready-serverless-workshop/tests/test_cases/notify-restaurant.tests.js
+++ production-ready-serverless-workshop/tests/test_cases/notify-restaurant.tests.js
@@ -1,12 +1,8 @@
 const { init } = require('../steps/init')
 const when = require('../steps/when')
-const AWS = require('aws-sdk')
 const chance = require('chance').Chance()
 const messages = require('../messages')
 console.log = jest.fn()
-
-const mockPutEvents = jest.fn()
-const mockPublish = jest.fn()
 
 describe(`When we invoke the notify-restaurant function`, () => {
     const event = {
@@ -20,58 +16,26 @@ describe(`When we invoke the notify-restaurant function`, () => {
 
     beforeAll(async () => {
         await init()
-
-        if (process.env.TEST_MODE === 'handler') {
-            AWS.EventBridge.prototype.putEvents = mockPutEvents
-            AWS.SNS.prototype.publish = mockPublish
-
-            mockPutEvents.mockReturnValue({
-                promise: async () => {}
-            })
-            mockPublish.mockReturnValue({
-                promise: async () => {}
-            })
-        } else {
-            messages.startListening()
-        }
-
+        messages.startListening()
         await when.we_invoke_notify_restaurant(event)
     })
 
-    afterAll(() => {
-        if (process.env.TEST_MODE === 'handler') {
-            mockPutEvents.mockClear()
-            mockPublish.mockClear()
-        }
-    })
+    it(`Should publish message to SNS`, async () => {
+        await messages.waitForMessage(
+            'sns',
+            process.env.RESTAURANT_NOTIFICATION_TOPIC_ARN,
+            JSON.stringify(event.detail)
+        )
+    }, 10000)
 
-    if (process.env.TEST_MODE === 'handler') {
-        it(`Should publish message to SNS`, async () => {
-            expect(mockPublish).toBeCalledWith({
-                Message: expect.stringMatching(`"restaurantName":"Fangtasia"`),
-                TopicArn: expect.stringMatching(process.env.restaurant_notification_topic)
+    it(`Should publish "restaurant_notified" event to EventBridge`, async () => {
+        await messages.waitForMessage(
+            'eventbridge',
+            process.env.EVENT_BUS_NAME,
+            JSON.stringify({
+                ...event,
+                'detail-type': 'restaurant_notified'
             })
-        })
-
-        it(`Should publish event to EventBridge`, async () => {
-            expect(mockPutEvents).toBeCalledWith({
-                Entries: [
-                    expect.objectContaining({
-                        Source: 'big-mouth',
-                        DetailType: 'restaurant_notified',
-                        Detail: expect.stringContaining(`"restaurantName":"Fangtasia"`),
-                        EventBusName: expect.stringMatching(process.env.bus_name)
-                    })
-                ]
-            })
-        })
-    } else {
-        it(`Should publish message to SNS`, async () => {
-            await messages.waitForMessage(
-                'sns',
-                process.env.RESTAURANT_NOTIFICATION_TOPIC_ARN,
-                JSON.stringify(event.detail)
-            )
-        }, 10000)
-    }
+        )
+    }, 10000)
 })
